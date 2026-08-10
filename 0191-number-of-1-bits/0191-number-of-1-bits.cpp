4ffb1e7b00940ee419a3e858324854ccb44f366d
malladisriharini <malladisriharini@gmail.com>
2026-08-10 14:35:50 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.29%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        /*int res=__builtin_popcount(n);
+        return res;
+        */
+int cnt=0;
+        while(n!=0){
+            if(n%2==1) cnt++;
+            n/=2;
+        }
+        return cnt;
+    }
+};
